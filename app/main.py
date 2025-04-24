0060d9327b6ca959ab54bf3ce30d6260718c28f5
CalsonicSSS <calson.developer@gmail.com>
2025-04-24 10:08:37 -0400

    sync up

--- app/main.py
+++ app/main.py
@@ -24,14 +24,23 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title=config_settings.PROJECT_NAME, version=config_settings.VERSION, lifespan=lifespan)
 
 # Middleware is applied globally to all requests that reach the FastAPI application (Dependencies are different, they are applied to specific endpoint)
+# You can add multiple middleware for different purposes
+# Middleware runs in the order it's added. If you add MiddlewareA then MiddlewareB, A processes the request first.
+
+# "add_middleware": A method to register middleware on your FastAPI app.
+# First argument is the middleware class (not an instance!), followed by its configuration (VERY IMPORTANT PATTERN IN fastapi).
+# Named arguments (allow_origins, allow_methods, etc.): These are specific to CORSMiddleware and define its behavior. Other middleware types (e.g., authentication) would take different arguments.
 app.add_middleware(
+    # CORSMiddleware A pre-built class provided by FastAPI It handles all CORS-related headers automatically.
     CORSMiddleware,
-    # these are 4 main checks setup for CORS, they are all part of the same middleware here.
+    # these are 4 main checks setup for CORS related middleware, they are all part of the SAME middleware here.
     allow_origins=config_settings.ALLOWED_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_credentials=True,  # Allows cookies/auth headers (e.g., for JWT).
+    allow_methods=["*"],  # HTTP methods permitted (e.g., ["GET", "POST"]).
     allow_headers=["*"],
 )
+
+# To add other middleware, you'd call app.add_middleware() again with a different class.
 
 app.include_router(suggestion_generation_router, prefix="/api/v1")
 app.include_router(payments_router, prefix="/api/v1")
